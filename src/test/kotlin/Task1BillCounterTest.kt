2819e85dec4ff0c4da637dfc9d341faa1c2508c5
UTMarckus <utmarckus@gmail.com>
2025-08-24 14:37:23 +0300

    feat: add subtask2 tests

---
+++ src/test/kotlin/Task1BillCounterTest.kt
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import subtask2.BillCounter
+
+class Task1BillCounterTest {
+
+    private val counter = BillCounter()
+    private val fairlySplit = "bon appetit"
+
+    @Test
+    fun testTask3BillCounter1() {
+        val billArray = intArrayOf(10, 1, 9)
+        assertEquals(fairlySplit, counter.calculateFairlySplit(billArray, 0, 5).lowercase())
+    }
+
+    @Test
+    fun testTask3BillCounter2() {
+        val billArray = intArrayOf(3, 10, 2, 9, 18, 25)
+        assertEquals(fairlySplit, counter.calculateFairlySplit(billArray, 3, 29).lowercase())
+    }
+
+    @Test
+    fun testTask3BillCounter3() {
+        val billArray = intArrayOf(3, 10, 2, 9)
+        assertEquals("5", counter.calculateFairlySplit(billArray, 1, 12).lowercase())
+    }
+
+    @Test
+    fun testTask3BillCounter4() {
+        val billArray = intArrayOf(3, 10, 2, 9, 18, 25, 45)
+        assertEquals("22", counter.calculateFairlySplit(billArray, 4, 69).lowercase())
+    }
+
+}
