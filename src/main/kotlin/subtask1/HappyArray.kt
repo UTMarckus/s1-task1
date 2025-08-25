67c1b2ad90e500057b783e6014d0f7f015e8452c
UTMarckus <utmarckus@gmail.com>
2025-08-25 11:56:20 +0300

    feat: add subtask1 HappyArray implementation

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -3,6 +3,29 @@ package subtask1
 class HappyArray {
 
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        var resArr = sadArray
+        var isHappy = false
+
+        do {
+            val startArray = resArr
+            resArr = intArrayOf()
+            isHappy = true
+
+            startArray.forEachIndexed { index, i ->
+                when {
+                    index == 0 || index == startArray.lastIndex -> resArr += i
+                    startArray[index - 1] + startArray[index + 1] > i -> resArr += i
+                    else -> isHappy = false
+                }
+            }
+
+        } while (!isHappy)
+
+        return resArr
     }
+}
+
+fun main() {
+    val sadArray = intArrayOf(1, 5, 2, 7, 93, 8, 9, 3)
+    println(HappyArray().convertToHappy(sadArray).joinToString())
 }
